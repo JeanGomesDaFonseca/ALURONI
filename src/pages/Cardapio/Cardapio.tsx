1355ea98cc408880bf2ba0edf78868f1c08ef889
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-10-20 18:45:52 -0300

    commit 3

--- src/pages/Cardapio/Cardapio.tsx
+++ src/pages/Cardapio/Cardapio.tsx
@@ -1,7 +1,11 @@
 import style from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador/Buscador";
+import { useState } from "react";
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState("");
+
   return (
     <main>
       <nav className={style.menu}>
@@ -10,6 +14,10 @@ export default function Cardapio() {
       <header className={style.header}>
         <div className={style.header__text}>A casa do código da massa</div>
       </header>
+      <section className={style.cardapio}>
+        <h3 className={style.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+      </section>
     </main>
   );
 }
