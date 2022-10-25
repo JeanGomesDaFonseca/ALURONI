84fd36eae40c03d5e4b8bdcb07c175688323c8ad
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-10-25 15:57:45 -0300

    Projeto finalizado. Agora preciso verificar os ajustes que terei que fazer em scss.

--- src/pages/Cardapio/Cardapio.tsx
+++ src/pages/Cardapio/Cardapio.tsx
@@ -26,7 +26,7 @@ export default function Cardapio() {
           <Filtros filtro={filtro} setFiltro={setFiltro} />
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
-        <Itens />
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
       </section>
     </main>
   );
--- src/pages/Cardapio/Itens/Itens.tsx
+++ src/pages/Cardapio/Itens/Itens.tsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Itens.module.scss";
 import cardapio from "./Itens.json";
 import Item from "./Item/Item";
+import Ordenador from "../Ordenador/Ordenador";
 
-export default function Itens() {
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+export default function Itens(props: Props) {
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro, ordenador } = props;
+
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, "i");
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if (filtro !== null) return filtro === id;
+    return true;
+  }
+
+  function ordenar(novaLista: typeof cardapio) {
+    switch (ordenador) {
+      case "porcao":
+        return novaLista.sort((a, b) => (a.size > b.size ? 1 : -1));
+      case "qtd_pessoas":
+        return novaLista.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+      case "preco":
+        return novaLista.sort((a, b) => (a.price > b.price ? 1 : -1));
+      default:
+        return novaLista;
+    }
+  }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(
+      (item) => testaBusca(item.title) && testaFiltro(item.category.id)
+    );
+    setLista(ordenar(novaLista));
+  }, [busca, filtro, ordenador]);
+
   return (
     <div className={style.itens}>
-      {cardapio.map((item) => (
+      {lista.map((item) => (
         <Item key={item.id} {...item} />
       ))}
     </div>
