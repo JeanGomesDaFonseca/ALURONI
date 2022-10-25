77b972003a1dfafcba4c68de7af0ef15abd5f0fc
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-10-25 13:53:52 -0300

    importando as imgs dos pratos

--- src/pages/Cardapio/Itens/Item/Item.tsx
+++ src/pages/Cardapio/Itens/Item/Item.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import style from "./Item.module.scss";
-import logo from "assets/logo.svg";
-import cardapio from "../Itens.json"
+import cardapio from "../Itens.json";
+import classNames from "classnames";
+
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props) {
-  const {title, description, category} = props;
+  const { title, description, category, size, serving, price, photo } = props;
   return (
     <div className={style.item}>
       <div className={style.item__imagem}>
-        <img src={logo} alt="imagem" />
+        <img src={photo} alt={title} />
       </div>
       <div className={style.item__descricao}>
         <div className={style.item__titulo}>
@@ -18,10 +19,19 @@ export default function Item(props: Props) {
           <p>{description}</p>
         </div>
         <div className={style.item__tags}>
-          <div className={style.item__tipo}>{category.label}</div>
-          <div className={style.item__porcao}>400g</div>
-          <div className={style.item__qtdpessoas}>Serve duas pessoas </div>
-          <div className={style.item__valor}>R$ 50</div>
+          <div
+            className={classNames({
+              [style.item__tipo]: true,
+              [style[`item__tipo__${category.label.toLowerCase()}`]]: true,
+            })}
+          >
+            {category.label}
+          </div>
+          <div className={style.item__porcao}>{size}g</div>
+          <div className={style.item__qtdpessoas}>
+            Serve {serving} pessoas {serving === 1 ? "" : "s"}{" "}
+          </div>
+          <div className={style.item__valor}>R$ {price.toFixed(2)}</div>
         </div>
       </div>
     </div>
