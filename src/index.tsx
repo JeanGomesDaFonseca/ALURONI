89a188c0ebcd781c581f7d7e9acc228381195cc8
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-10-26 17:23:36 -0300

    Criando Routers e aprendendo como funciona.

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio/Inicio';
+import Router from './routes';
+
+const componenteAtual = window.location.pathname === '/' ? <Inicio/> : <Cardapio/>
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardapio />
+    <Router/>
   </React.StrictMode>,
   document.getElementById('root')
 );
