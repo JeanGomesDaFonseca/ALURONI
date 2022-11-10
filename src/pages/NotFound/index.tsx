9fa2bf1dba4ccd211a824c714e1471814e7aa820
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-11-10 14:23:25 -0300

    commit 1

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,8 +2,11 @@ import styles from "./NotFound.module.scss";
 import { ReactComponent as NotFoundImg } from "assets/not_found.svg";
 import classNames from "classnames";
 import stylesTema from "styles/Tema.module.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <div
       className={classNames({
@@ -11,6 +14,9 @@ export default function NotFound() {
         [stylesTema.container]: true,
       })}
     >
+      <div className={styles.voltar}>
+        <button onClick={() => navigate(-1)}>{"< Voltar"}</button>
+      </div>
       <NotFoundImg />
     </div>
   );
