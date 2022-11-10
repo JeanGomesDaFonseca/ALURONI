75c67d5bb60acab1d866073d641eb7a2c55e862f
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-11-10 12:00:45 -0300

    criando a página de NotFound.

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,17 @@
-import styles from './NotFound.module.scss'
+import styles from "./NotFound.module.scss";
+import { ReactComponent as NotFoundImg } from "assets/not_found.svg";
+import classNames from "classnames";
+import stylesTema from "styles/Tema.module.scss";
 
-export default function NotFound(){
-    return(
-<div>
-    
-</div>
-    )
+export default function NotFound() {
+  return (
+    <div
+      className={classNames({
+        [styles.container]: true,
+        [stylesTema.container]: true,
+      })}
+    >
+      <NotFoundImg />
+    </div>
+  );
 }
