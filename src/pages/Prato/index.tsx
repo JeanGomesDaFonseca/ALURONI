ec25caf108de7cd62df373734f9d917297008364
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-11-11 11:11:15 -0300

    reorganizando os arquivos de pratos e itens.

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,18 +1,20 @@
 import styles from "./Prato.module.scss";
 import { useParams, useNavigate } from "react-router-dom";
-import classNames from "classnames";
 import cardapio from "data/cardapio.json";
+import TagsPrato from "components/TagsPrato";
 
 export default function Pratos() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato){
-    return'';
+  const prato = cardapio.find((item) => item.id === Number(id));
+  if (!prato) {
+    return "";
   }
   return (
     <>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>{"< Voltar"}</button>
+      <button className={styles.voltar} onClick={() => navigate(-1)}>
+        {"< Voltar"}
+      </button>
       <section className={styles.container}>
         <h1 className={styles.titulo}>{prato.title}</h1>
         <div className={styles.imagem}>
@@ -20,24 +22,7 @@ export default function Pratos() {
         </div>
         <div className={styles.conteudo}>
           <p className={styles.conteudo__descricao}>{prato.description}</p>
-          <div className={styles.tags}>
-            <div
-              className={classNames({
-                [styles.tags__tipo]: true,
-                [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]:
-                  true,
-              })}
-            >
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>{prato.size}g</div>
-            <div className={styles.tags__qtdPessoas}>
-              Serve {prato.serving} pessoa{prato.serving === 1 ? "" : "s"}
-            </div>
-            <div className={styles.tags__valor}>
-              R$ {prato.price.toFixed(2)}
-            </div>
-          </div>
+          <TagsPrato {...prato} />
         </div>
       </section>
     </>
